4ac08cd954169d4d7175bffe1135f185d0234bc3
J7 <j7.dev.gg@outlook.com>
2023-06-10 20:00:17 +0800

    feat: add wc-store TProduct type

--- js/src/types/wcStoreApi/product.ts
+++ js/src/types/wcStoreApi/product.ts
@@ -8,7 +8,34 @@ type TImage = {
   alt: string
 }
 
-export type TProductBase = {
+type TProductTerm = {
+  id: number
+  name: string
+  slug: string
+  link: string
+}
+
+type TProductAttribute = {
+  id: number
+  name: string
+  taxonomy: string
+  has_variations: boolean
+  terms: {
+    id: number
+    name: string
+    slug: string
+  }[]
+}
+
+type TProductVariation = {
+  id: number
+  attributes: {
+    name: string
+    value: string
+  }[]
+}
+
+export type TProduct = {
   id: number
   name: string
   parent: number
@@ -39,157 +66,23 @@ export type TProductBase = {
   average_rating: string
   review_count: number
   images: TImage[]
-}
-
-export type TProduct = TProductBase & {
-  categories: [
-    {
-      id: 19
-      name: 'Accessories'
-      slug: 'accessories'
-      link: 'http://wpdev.local/product-category/clothing/accessories/'
-    },
-  ]
-  tags: []
-  attributes: []
-  variations: []
-  has_options: false
-  is_purchasable: true
-  is_in_stock: true
-  is_on_backorder: false
-  low_stock_remaining: null
-  sold_individually: false
+  categories: TProductTerm[]
+  tags: TProductTerm[]
+  attributes: TProductAttribute[]
+  variations: TProductVariation[]
+  has_options: boolean
+  is_purchasable: boolean
+  is_in_stock: boolean
+  is_on_backorder: boolean
+  low_stock_remaining: any
+  sold_individually: boolean
   add_to_cart: {
-    text: 'Add to cart'
-    description: 'Add &ldquo;Belt&rdquo; to your cart'
-    url: '?add-to-cart=17'
-    minimum: 1
-    maximum: 9999
-    multiple_of: 1
-  }
-  extensions: {}
-}
-
-export type TProductVariation = TProductBase & {
-  categories: [
-    {
-      id: 18
-      name: 'Hoodies'
-      slug: 'hoodies'
-      link: 'http://wpdev.local/product-category/clothing/hoodies/'
-    },
-  ]
-  tags: []
-  attributes: [
-    {
-      id: 1
-      name: 'Color'
-      taxonomy: 'pa_color'
-      has_variations: true
-      terms: [
-        {
-          id: 22
-          name: 'Blue'
-          slug: 'blue'
-        },
-        {
-          id: 23
-          name: 'Green'
-          slug: 'green'
-        },
-        {
-          id: 24
-          name: 'Red'
-          slug: 'red'
-        },
-      ]
-    },
-    {
-      id: 0
-      name: 'Logo'
-      taxonomy: null
-      has_variations: true
-      terms: [
-        {
-          id: 0
-          name: 'Yes'
-          slug: 'Yes'
-        },
-        {
-          id: 0
-          name: 'No'
-          slug: 'No'
-        },
-      ]
-    },
-  ]
-  variations: [
-    {
-      id: 36
-      attributes: [
-        {
-          name: 'Color'
-          value: 'blue'
-        },
-        {
-          name: 'Logo'
-          value: 'Yes'
-        },
-      ]
-    },
-    {
-      id: 29
-      attributes: [
-        {
-          name: 'Color'
-          value: 'red'
-        },
-        {
-          name: 'Logo'
-          value: 'No'
-        },
-      ]
-    },
-    {
-      id: 30
-      attributes: [
-        {
-          name: 'Color'
-          value: 'green'
-        },
-        {
-          name: 'Logo'
-          value: 'No'
-        },
-      ]
-    },
-    {
-      id: 31
-      attributes: [
-        {
-          name: 'Color'
-          value: 'blue'
-        },
-        {
-          name: 'Logo'
-          value: 'No'
-        },
-      ]
-    },
-  ]
-  has_options: true
-  is_purchasable: true
-  is_in_stock: true
-  is_on_backorder: false
-  low_stock_remaining: null
-  sold_individually: false
-  add_to_cart: {
-    text: 'Select options'
-    description: 'Select options for &ldquo;Hoodie&rdquo;'
-    url: 'http://wpdev.local/product/hoodie/'
-    minimum: 1
-    maximum: 9999
-    multiple_of: 1
+    text: string
+    description: string
+    url: string
+    minimum: number
+    maximum: number
+    multiple_of: number
   }
   extensions: {}
 }
