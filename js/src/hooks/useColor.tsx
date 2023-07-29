cce3163a008aa4cae171669729037b632640c122
J7 <j7.dev.gg@outlook.com>
2023-07-29 20:35:13 +0800

    feat: add useColor

---
+++ js/src/hooks/useColor.tsx
@@ -0,0 +1,10 @@
+import { theme } from 'antd'
+
+const { useToken } = theme
+
+const useColor = () => {
+  const { token } = useToken()
+  return token
+}
+
+export default useColor
--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Author: j7.dev
  * Author URI: https://github.com/j7-dev
  * License: GPLv2
- * Version: 1.2.5
+ * Version: 1.2.6
  * Requires PHP: 8.1
  */
 
