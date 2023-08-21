5673518e63180e0bf24eb30967f9d451e39cc5d6
TW-IT-Jerry Liu <jerry.liu@shoalter.com>
2023-08-21 13:33:03 +0800

    feat: add html to shortcode

--- inc/custom/class/class-shortcode.php
+++ inc/custom/class/class-shortcode.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace J7\ViteReactWPPlugin\PowerShop\Admin;
 
-use J7\ViteReactWPPlugin\PowerShop\Admin\Bootstrap;
-
 class ShortCode
 {
 	function __construct()
@@ -25,10 +23,28 @@ class ShortCode
 			}
 		}
 
+		// 取得 power shop meta 資料
+		global $post;
+		$meta_string = \get_post_meta($post->ID, $_ENV['SNAKE'] . '_meta', true);
+		$shop_meta = Functions::json_parse($meta_string);
+
 
 		$html = '';
 		ob_start();
 ?>
+		<div class="power-shop-products">
+			<div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
+				<div class="relative pb-12 cursor-pointer">
+					<div>
+						<img src="https://projectmars.shop/wp-content/uploads/2021/05/PNG去背.png" class="w-full aspect-square">
+					</div>
+					<div class="m-0">
+						<div>Booster MINI 3<br>肌肉放鬆迷你強力筋膜槍</div>
+					</div>
+					<div>$Infinity – $-Infinity</div><button type="button" class="ant-btn css-fpg3f5 ant-btn-primary w-full absolute bottom-0"><span>加入購物車</span></button>
+				</div>
+			</div>
+		</div>
 		<div id="<?= $_ENV['VITE_RENDER_ID_3'] ?>"></div>
 <?php
 		$html .= ob_get_clean();
