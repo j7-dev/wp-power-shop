b09b5b922ee9a41f388aaac6877c7bbf8c1f02b8
J7 <j7.dev.gg@outlook.com>
2023-11-16 00:07:10 +0900

    feat: 沒有可用的運送方法時 隱藏運送欄

--- js/src/pages/PowerShopProducts/Cart/ShippingField.tsx
+++ js/src/pages/PowerShopProducts/Cart/ShippingField.tsx
@@ -9,30 +9,19 @@ import { useQueryClient } from '@tanstack/react-query'
 import { InfoCircleFilled } from '@ant-design/icons'
 import { getPrice } from '@/utils'
 
-const renderItem = (
-  shipping_rate: TShippingRates,
-  currency_minor_unit: number,
-) => {
-  const price = getPrice(
-    parseInt(shipping_rate?.price ?? '0', 10),
-    currency_minor_unit,
-  )
+const renderItem = (shipping_rate: TShippingRates, currency_minor_unit: number) => {
+  const price = getPrice(parseInt(shipping_rate?.price ?? '0', 10), currency_minor_unit)
 
   return (
     <div className="flex items-center justify-between">
-      <div className="flex items-center">
-        {shipping_rate?.name ?? '未知運送方式'}
-      </div>
+      <div className="flex items-center">{shipping_rate?.name ?? '未知運送方式'}</div>
 
       <div>{`$ ${price}`}</div>
     </div>
   )
 }
 
-const ShippingField: React.FC<{ cartData: TCart; isLoading: boolean }> = ({
-  cartData,
-  isLoading,
-}) => {
+const ShippingField: React.FC<{ cartData: TCart; isLoading: boolean }> = ({ cartData, isLoading }) => {
   const shipments = cartData?.shipping_rates ?? []
   const currency_minor_unit = cartData?.totals?.currency_minor_unit || 0
   const package_id = shipments?.[0]?.package_id ?? 0
@@ -40,10 +29,7 @@ const ShippingField: React.FC<{ cartData: TCart; isLoading: boolean }> = ({
   // const shipmentName = shipments?.[0]?.name ?? ''
 
   const shipping_rates = shipments?.[0]?.shipping_rates ?? []
-  const raw_total_shipping = parseInt(
-    cartData?.totals?.total_shipping || '0',
-    10,
-  )
+  const raw_total_shipping = parseInt(cartData?.totals?.total_shipping || '0', 10)
   const total_shipping = getPrice(raw_total_shipping, currency_minor_unit)
   const storeApiNonce = useAtomValue(storeApiNonceAtom)
   const queryClient = useQueryClient()
@@ -61,9 +47,7 @@ const ShippingField: React.FC<{ cartData: TCart; isLoading: boolean }> = ({
     },
   ]
 
-  const defaultOption =
-    shipping_rates.find((shipping_rate) => shipping_rate.selected)?.rate_id ??
-    ''
+  const defaultOption = shipping_rates.find((shipping_rate) => shipping_rate.selected)?.rate_id ?? ''
 
   const { mutate, isLoading: updateIsLoading } = useUpdate({
     resource: 'cart/select-shipping-rate',
@@ -89,19 +73,15 @@ const ShippingField: React.FC<{ cartData: TCart; isLoading: boolean }> = ({
     )
   }
 
+  if (options.length === 0) {
+    return null
+  }
+
   return (
     <tr>
       <th className="text-left pl-4" colSpan={3}>
         <span className="mr-2">運費</span>
-        <Select
-          className="min-w-[12rem]"
-          defaultValue={nonOptions[0].value}
-          value={options.length > 0 ? defaultOption : nonOptions[0].value}
-          options={options.length > 0 ? options : nonOptions}
-          onChange={handleChange}
-          loading={isLoading}
-          disabled={options.length === 0}
-        />
+        <Select className="min-w-[12rem]" defaultValue={nonOptions[0].value} value={options.length > 0 ? defaultOption : nonOptions[0].value} options={options.length > 0 ? options : nonOptions} onChange={handleChange} loading={isLoading} disabled={options.length === 0} />
         {options.length === 0 && (
           <Tooltip title='請確認您的地址"國家"欄位有可用的運送方式或者確認購物車有商品'>
             <InfoCircleFilled className="ml-2 text-orange-500" />
@@ -109,11 +89,7 @@ const ShippingField: React.FC<{ cartData: TCart; isLoading: boolean }> = ({
         )}
       </th>
       <th className="text-right">
-        <LoadingText
-          width="w-[4rem]"
-          content={`$ ${total_shipping.toLocaleString()}`}
-          isLoading={isLoading || updateIsLoading}
-        />
+        <LoadingText width="w-[4rem]" content={`$ ${total_shipping.toLocaleString()}`} isLoading={isLoading || updateIsLoading} />
       </th>
       <th></th>
     </tr>
