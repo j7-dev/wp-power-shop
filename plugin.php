5e02f98806c1dde4310de535ca6c2bce5fde1ed4
J7 <j7.dev.gg@outlook.com>
2023-11-21 17:07:30 +0800

    feat: add plugin update checker

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Power Shop | 讓你的商店充滿 Power
  * Plugin URI:        https://cloud.luke.cafe/plugins/power-shop/
  * Description:       Power Shop 是一個 WordPress 套件，安裝後，可以讓你的 Woocommerce 商店變成可以提供給多人使用的一頁商店，並且可以讓使用者自訂商品的價格，以及統計每個一頁商店的訂單狀態與銷售額
- * Version:           1.0.10
+ * Version:           1.0.11
  * Requires at least: 5.7
  * Requires PHP:      7.4
  * Author:            j7.dev
@@ -18,6 +18,22 @@
 
 require_once "inc/index.php";
 require_once "licenser/class-power-shop-base.php";
+
+/**
+ * wp plugin 更新檢查 update checker
+ */
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$updateChecker = PucFactory::buildUpdateChecker(
+	$_ENV['GITHUB_REPO'],
+	__FILE__,
+	$_ENV['APP_SLUG']
+);
+$updateChecker->setAuthentication('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');
+
+$updateChecker->getVcsApi()->enableReleaseAssets();
+
 
 class PowerShop
 {
