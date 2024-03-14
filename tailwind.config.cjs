5045696a0500de9a923513bb3dc24465c7f2be4e
J7 <j7.dev.gg@outlook.com>
2024-03-14 23:09:16 +0800

    fix: turn  tailwindcss to important

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  important: '.tailwindcss',
+  important: true,
   corePlugins: {
     preflight: false,
   },
