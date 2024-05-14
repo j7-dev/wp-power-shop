ff90f7d0d7b551ac2ed636b68e3e860051f1abfa
J7 <j7.dev.gg@outlook.com>
2024-05-15 01:18:24 +0800

    feat: no licensor

--- inc/custom/class/class-cpt.php
+++ inc/custom/class/class-cpt.php
@@ -6,239 +6,236 @@ namespace J7\WpReactPlugin\PowerShop\Inc;
 
 use J7\WpReactPlugin\PowerShop\Inc\Bootstrap;
 
-class CPT extends Bootstrap
-{
-    const VAR          = 'ps_report';
-    const POST_META    = [ 'meta', 'settings' ];
-    const CPT_LABEL    = 'Power Shop';
-    const CPT_SLUG     = 'power-shop';
-    const LICENSE_LINK = 'edit.php?post_type=power-shop&page=power-shop-license';
-    const COLOR        = '#72aee6';
+class CPT extends Bootstrap {
 
-    const SNAKE            = Bootstrap::SNAKE;
-    const KEBAB            = Bootstrap::KEBAB;
-    const RENDER_ID_1      = Bootstrap::RENDER_ID_1;
-    const RENDER_ID_2      = Bootstrap::RENDER_ID_2;
-    const BUY_LICENSE_LINK = Bootstrap::BUY_LICENSE_LINK;
-    const SUPPORT_EMAIL    = Bootstrap::SUPPORT_EMAIL;
+	const VAR          = 'ps_report';
+	const POST_META    = array( 'meta', 'settings' );
+	const CPT_LABEL    = 'Power Shop';
+	const CPT_SLUG     = 'power-shop';
+	const LICENSE_LINK = 'edit.php?post_type=power-shop&page=power-shop-license';
+	const COLOR        = '#72aee6';
 
-    private $count_publish = 0;
-    private $iel           = false;
+	const SNAKE            = Bootstrap::SNAKE;
+	const KEBAB            = Bootstrap::KEBAB;
+	const RENDER_ID_1      = Bootstrap::RENDER_ID_1;
+	const RENDER_ID_2      = Bootstrap::RENDER_ID_2;
+	const BUY_LICENSE_LINK = Bootstrap::BUY_LICENSE_LINK;
+	const SUPPORT_EMAIL    = Bootstrap::SUPPORT_EMAIL;
 
-    public function __construct()
-    {
-        \add_action('init', [ $this, 'init' ]);
-        \add_action('rest_api_init', [ $this, 'add_post_meta' ]);
+	private $count_publish = 0;
+	private $iel           = false;
 
-        \add_action('load-post.php', [ $this, 'init_metabox' ]);
-        \add_action('load-post-new.php', [ $this, 'init_metabox' ]);
+	public function __construct() {
+		\add_action( 'init', array( $this, 'init' ) );
+		\add_action( 'rest_api_init', array( $this, 'add_post_meta' ) );
 
-        \add_filter('query_vars', [ $this, 'add_query_for_report' ]);
-        \add_filter('template_include', [ $this, 'load_report_template' ], 999);
+		\add_action( 'load-post.php', array( $this, 'init_metabox' ) );
+		\add_action( 'load-post-new.php', array( $this, 'init_metabox' ) );
 
-        \add_action('wp_insert_post', [ $this, 'set_default_power_shop_meta' ], 10, 3);
+		\add_filter( 'query_vars', array( $this, 'add_query_for_report' ) );
+		\add_filter( 'template_include', array( $this, 'load_report_template' ), 999 );
 
-        \add_action('publish_' . self::CPT_SLUG, [ $this, 'post_published_limit' ], 999, 3);
-        \add_filter('post_row_actions', [ $this, 'remove_row_actions' ], 999, 2);
-        \add_filter("bulk_actions-edit-" . self::CPT_SLUG, [ $this, 'remove_bulk_actions' ], 999, 1);
+		\add_action( 'wp_insert_post', array( $this, 'set_default_power_shop_meta' ), 10, 3 );
 
-        \add_action('admin_enqueue_scripts', [ $this, 'limit_css_and_js' ], 999);
-        \add_action('admin_head', [ $this, 'limit_admin_head' ], 999, 1);
-        \add_action('admin_notices', [ $this, 'limit_admin_notices' ], 999);
-    }
+		\add_action( 'publish_' . self::CPT_SLUG, array( $this, 'post_published_limit' ), 999, 3 );
+		\add_filter( 'post_row_actions', array( $this, 'remove_row_actions' ), 999, 2 );
+		\add_filter( 'bulk_actions-edit-' . self::CPT_SLUG, array( $this, 'remove_bulk_actions' ), 999, 1 );
 
-    public function add_query_for_report($vars)
-    {
-        $vars[  ] = self::VAR;
-        return $vars;
-    }
+		// \add_action('admin_enqueue_scripts', [ $this, 'limit_css_and_js' ], 999);
+		// \add_action('admin_head', [ $this, 'limit_admin_head' ], 999, 1);
+		// \add_action('admin_notices', [ $this, 'limit_admin_notices' ], 999);
+	}
 
-    public function init(): void
-    {
-        Functions::register_cpt(self::CPT_LABEL);
+	public function add_query_for_report( $vars ) {
+		$vars[] = self::VAR;
+		return $vars;
+	}
 
-        // 新增 {self::KEBAB}/{slug}/report 網址規則
-        \add_rewrite_rule('^' . self::CPT_SLUG . '/([^/]+)/report/?$', 'index.php?post_type=' . self::CPT_SLUG . '&name=$matches[1]&' . self::VAR  . '=1', 'top');
+	public function init(): void {
+		Functions::register_cpt( self::CPT_LABEL );
 
-        \flush_rewrite_rules();
+		// 新增 {self::KEBAB}/{slug}/report 網址規則
+		\add_rewrite_rule( '^' . self::CPT_SLUG . '/([^/]+)/report/?$', 'index.php?post_type=' . self::CPT_SLUG . '&name=$matches[1]&' . self::VAR . '=1', 'top' );
 
-        $info = \Power_Shop_Base::get_register_info();
-        if (@$info?->is_valid) {
-            return;
-        }
+		\flush_rewrite_rules();
 
-        $count_posts         = \wp_count_posts(self::CPT_SLUG);
-        $this->count_publish = $count_posts->publish;
+		$info = \Power_Shop_Base::get_register_info();
+		// if (@$info->is_valid) {
+			return;
+		// }
 
-        if (AXD::gt($this->count_publish)) {
-            $this->iel = true;
-        }
-    }
+		// $count_posts         = \wp_count_posts(self::CPT_SLUG);
+		// $this->count_publish = $count_posts->publish;
 
-    public function add_post_meta(): void
-    {
-        foreach (self::POST_META as $meta_key) {
-            \register_meta('post', self::SNAKE . '_' . $meta_key, [
-                'type'         => 'string',
-                'show_in_rest' => true,
-                'single'       => true,
-             ]);
-        }
-    }
+		// if (AXD::gt($this->count_publish)) {
+		// $this->iel = true;
+		// }
+	}
 
-    /**
-     * Meta box initialization.
-     */
-    public function init_metabox(): void
-    {
-        \add_action('add_meta_boxes', [ $this, 'add_metaboxs' ]);
-        \add_filter('rewrite_rules_array', [ $this, 'custom_post_type_rewrite_rules' ]);
-    }
+	public function add_post_meta(): void {
+		foreach ( self::POST_META as $meta_key ) {
+			\register_meta(
+				'post',
+				self::SNAKE . '_' . $meta_key,
+				array(
+					'type'         => 'string',
+					'show_in_rest' => true,
+					'single'       => true,
+				)
+			);
+		}
+	}
 
-    public function custom_post_type_rewrite_rules($rules)
-    {
-        global $wp_rewrite;
-        if (is_object($wp_rewrite) && !is_null($wp_rewrite)) {
-            $wp_rewrite->flush_rules();
-        }
-        return $rules;
-    }
+	/**
+	 * Meta box initialization.
+	 */
+	public function init_metabox(): void {
+		\add_action( 'add_meta_boxes', array( $this, 'add_metaboxs' ) );
+		\add_filter( 'rewrite_rules_array', array( $this, 'custom_post_type_rewrite_rules' ) );
+	}
 
-    /**
-     * Adds the meta box.
-     */
-    public function add_metaboxs(): void
-    {
-        Functions::add_metabox([
-            'id'    => self::RENDER_ID_1,
-            'label' => __('Added Products', self::KEBAB),
-         ]);
-        Functions::add_metabox([
-            'id'    => self::RENDER_ID_2,
-            'label' => __('Sales Stats', self::KEBAB),
-         ]);
-    }
+	public function custom_post_type_rewrite_rules( $rules ) {
+		global $wp_rewrite;
+		if ( is_object( $wp_rewrite ) && ! is_null( $wp_rewrite ) ) {
+			$wp_rewrite->flush_rules();
+		}
+		return $rules;
+	}
 
-    /**
-     * 設定 {self::KEBAB}/{slug}/report 的 php template
-     */
-    public function load_report_template($template)
-    {
-        $repor_template_path = Bootstrap::get_plugin_dir() . '/inc/templates/report.php';
+	/**
+	 * Adds the meta box.
+	 */
+	public function add_metaboxs(): void {
+		Functions::add_metabox(
+			array(
+				'id'    => self::RENDER_ID_1,
+				'label' => __( 'Added Products', self::KEBAB ),
+			)
+		);
+		Functions::add_metabox(
+			array(
+				'id'    => self::RENDER_ID_2,
+				'label' => __( 'Sales Stats', self::KEBAB ),
+			)
+		);
+	}
 
-        if (\get_query_var(self::VAR )) {
-            if (file_exists($repor_template_path)) {
-                return $repor_template_path;
-            }
-        }
-        return $template;
-    }
+	/**
+	 * 設定 {self::KEBAB}/{slug}/report 的 php template
+	 */
+	public function load_report_template( $template ) {
+		$repor_template_path = Bootstrap::get_plugin_dir() . '/inc/templates/report.php';
 
-    /**
-     * 設定預設的 post meta data
-     */
-    public function set_default_power_shop_meta($post_id, $post, $update)
-    {
-        // Get the post object
-        $post = \get_post($post_id);
+		if ( \get_query_var( self::VAR ) ) {
+			if ( file_exists( $repor_template_path ) ) {
+				return $repor_template_path;
+			}
+		}
+		return $template;
+	}
 
-        // 剛創建時，且 post type === self::KEBAB
-        if (!$update && $post->post_type === self::CPT_SLUG) {
-            // Add default post_meta
-            $default_password   = \wp_create_nonce(self::KEBAB);
-            $encrypted_password = base64_encode($default_password);
+	/**
+	 * 設定預設的 post meta data
+	 */
+	public function set_default_power_shop_meta( $post_id, $post, $update ) {
+		// Get the post object
+		$post = \get_post( $post_id );
 
-            \add_post_meta($post_id, self::SNAKE . '_report_password', $encrypted_password, true);
-            \add_post_meta($post_id, self::SNAKE . '_meta', '[]', true);
-        }
-    }
+		// 剛創建時，且 post type === self::KEBAB
+		if ( ! $update && $post->post_type === self::CPT_SLUG ) {
+			// Add default post_meta
+			$default_password   = \wp_create_nonce( self::KEBAB );
+			$encrypted_password = base64_encode( $default_password );
 
-    public function post_published_limit($post_id, $post, $old_status)
-    {
-        if ($this->iel) {
-            $post = array('post_status' => 'draft');
-            \wp_update_post($post);
-        }
-    }
+			\add_post_meta( $post_id, self::SNAKE . '_report_password', $encrypted_password, true );
+			\add_post_meta( $post_id, self::SNAKE . '_meta', '[]', true );
+		}
+	}
 
-    public function remove_row_actions($actions, $post)
-    {
-        if (self::CPT_SLUG === $post->post_type) {
-            unset($actions[ 'inline hide-if-no-js' ]);
-            return $actions;
-        }
-        return $actions;
-    }
+	public function post_published_limit( $post_id, $post, $old_status ) {
+		if ( $this->iel ) {
+			$post = array( 'post_status' => 'draft' );
+			\wp_update_post( $post );
+		}
+	}
 
-    public function remove_bulk_actions($actions)
-    {
-        unset($actions[ 'edit' ]);
-        return $actions;
-    }
+	public function remove_row_actions( $actions, $post ) {
+		if ( self::CPT_SLUG === $post->post_type ) {
+			unset( $actions['inline hide-if-no-js'] );
+			return $actions;
+		}
+		return $actions;
+	}
 
-    public function limit_admin_head()
-    {
-        $screen = \get_current_screen();
-        if ('edit-' . self::CPT_SLUG !== $screen->id) {
-            return;
-        }
+	public function remove_bulk_actions( $actions ) {
+		unset( $actions['edit'] );
+		return $actions;
+	}
 
-        $shop_ids = \get_posts(
-            array(
-                'post_type'      => self::CPT_SLUG,
-                'post_status'    => 'publish',
-                'fields'         => 'ids',
-                'posts_per_page' => -1,
-            )
-        );
+	public function limit_admin_head() {
+		$screen = \get_current_screen();
+		if ( 'edit-' . self::CPT_SLUG !== $screen->id ) {
+			return;
+		}
 
-        if ($this->iel && !empty($shop_ids)) {
-            foreach ($shop_ids as $key => $shop_id) {
-                if ($key !== 0) {
-                    \wp_update_post(
-                        array(
-                            'ID'          => $shop_id,
-                            'post_status' => 'draft',
-                        )
-                    );
-                }
-            }
-        }
-    }
+		$shop_ids = \get_posts(
+			array(
+				'post_type'      => self::CPT_SLUG,
+				'post_status'    => 'publish',
+				'fields'         => 'ids',
+				'posts_per_page' => -1,
+			)
+		);
 
-    public function limit_css_and_js()
-    {
-        if (AXD::gte($this->count_publish)) {
-            \wp_enqueue_style(self::CPT_SLUG, Bootstrap::get_plugin_url() . '/inc/assets/css/main.css');
-            \wp_enqueue_style('jquery-confirm', Bootstrap::get_plugin_url() . '/inc/assets/packages/jquery-confirm/jquery-confirm.min.css');
-            \wp_enqueue_script('jquery-confirm', Bootstrap::get_plugin_url() . '/inc/assets/packages/jquery-confirm/jquery-confirm.min.js', array('jquery'), '3.3.4', true);
-            \wp_enqueue_script(self::CPT_SLUG, Bootstrap::get_plugin_url() . '/inc/assets/js/main.js', array('jquery-confirm'), Bootstrap::get_plugin_ver(), true);
-            wp_localize_script(self::CPT_SLUG, 'powerShopData', [
-                'buyLink'      => self::BUY_LICENSE_LINK,
-                'licenseLink'  => \admin_url(self::LICENSE_LINK),
-                'supportEmail' => self::SUPPORT_EMAIL,
-             ]);
-        }
-    }
+		if ( $this->iel && ! empty( $shop_ids ) ) {
+			foreach ( $shop_ids as $key => $shop_id ) {
+				if ( $key !== 0 ) {
+					\wp_update_post(
+						array(
+							'ID'          => $shop_id,
+							'post_status' => 'draft',
+						)
+					);
+				}
+			}
+		}
+	}
 
-    public function limit_admin_notices()
-    {
-        $screen = \get_current_screen();
-        if ('edit-' . self::CPT_SLUG !== $screen->id) {
-            return;
-        }
+	public function limit_css_and_js() {
+		if ( AXD::gte( $this->count_publish ) ) {
+			\wp_enqueue_style( self::CPT_SLUG, Bootstrap::get_plugin_url() . '/inc/assets/css/main.css' );
+			\wp_enqueue_style( 'jquery-confirm', Bootstrap::get_plugin_url() . '/inc/assets/packages/jquery-confirm/jquery-confirm.min.css' );
+			\wp_enqueue_script( 'jquery-confirm', Bootstrap::get_plugin_url() . '/inc/assets/packages/jquery-confirm/jquery-confirm.min.js', array( 'jquery' ), '3.3.4', true );
+			\wp_enqueue_script( self::CPT_SLUG, Bootstrap::get_plugin_url() . '/inc/assets/js/main.js', array( 'jquery-confirm' ), Bootstrap::get_plugin_ver(), true );
+			wp_localize_script(
+				self::CPT_SLUG,
+				'powerShopData',
+				array(
+					'buyLink'      => self::BUY_LICENSE_LINK,
+					'licenseLink'  => \admin_url( self::LICENSE_LINK ),
+					'supportEmail' => self::SUPPORT_EMAIL,
+				)
+			);
+		}
+	}
 
-        $info = \Power_Shop_Base::get_register_info();
-        if (@$info?->is_valid) {
-            return;
-        }
+	public function limit_admin_notices() {
+		$screen = \get_current_screen();
+		if ( 'edit-' . self::CPT_SLUG !== $screen->id ) {
+			return;
+		}
 
-        $buy_link      = self::BUY_LICENSE_LINK;
-        $license_link  = \admin_url(self::LICENSE_LINK);
-        $support_email = self::SUPPORT_EMAIL;
-        $color         = self::COLOR;
+		$info = \Power_Shop_Base::get_register_info();
+		// if (@$info->is_valid) {
+			return;
+		// }
 
-        $html = <<<EOD
+		$buy_link      = self::BUY_LICENSE_LINK;
+		$license_link  = \admin_url( self::LICENSE_LINK );
+		$support_email = self::SUPPORT_EMAIL;
+		$color         = self::COLOR;
+
+		$html = <<<EOD
 		<div id='power-shop-reminder' class="notice notice-info is-dismissible" style="border-left-color:$color;">
 			<div class="e-notice__content">
 				<h3>升級PowerShop，讓你的商店更有POWER!</h3>
@@ -255,8 +252,8 @@ class CPT extends Bootstrap
 			</div>
 		</div>
 EOD;
-        echo $html;
-    }
+		echo $html;
+	}
 }
 
 new CPT();
