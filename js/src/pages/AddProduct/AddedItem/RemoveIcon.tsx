a829dd2431837c287314c6a2bc0c35243dab5492
J7 <j7.dev.gg@gmail.com>
2024-08-02 11:31:59 +0800

    fix: 解決刪除商品價格排序錯亂的問題

--- js/src/pages/AddProduct/AddedItem/RemoveIcon.tsx
+++ js/src/pages/AddProduct/AddedItem/RemoveIcon.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react'
 import { CloseCircleFilled } from '@ant-design/icons'
 import { useAtom, useSetAtom } from 'jotai'
 import { addedProductsAtom, isChangeAtom } from '../atoms'
+import { Form } from 'antd'
+import { TPSMeta } from '@/types'
 
 const RemoveIcon: FC<{ productId: number }> = ({ productId }) => {
   const [
@@ -9,21 +11,21 @@ const RemoveIcon: FC<{ productId: number }> = ({ productId }) => {
     setAddedProducts,
   ] = useAtom(addedProductsAtom)
   const setIsChange = useSetAtom(isChangeAtom)
+  const form = Form.useFormInstance()
 
   const handleRemoveProduct = () => {
-    const newAddedProducts = addedProducts.filter(
-      (item) => item.id !== productId,
-    )
+    const newAddedProducts = addedProducts.filter((item) => item.id !== productId)
     setAddedProducts(newAddedProducts)
     setIsChange(true)
+
+    // 同時也要改變表單順序
+    const values = form.getFieldsValue() // 舊的
+    const valuesInArray: TPSMeta[] = Object.values(values)
+    const newValuesInArray = valuesInArray.filter((item) => item.productId !== productId)
+    form.setFieldsValue(newValuesInArray)
   }
 
-  return (
-    <CloseCircleFilled
-      className="text-red-500 text-2xl cursor-pointer"
-      onClick={handleRemoveProduct}
-    />
-  )
+  return <CloseCircleFilled className="text-red-500 text-2xl cursor-pointer" onClick={handleRemoveProduct} />
 }
 
 export default RemoveIcon
