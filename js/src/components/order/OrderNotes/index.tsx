2e05e921379402a67552746c4443f94c789f4613
J7 <j7.dev.gg@gmail.com>
2025-03-13 18:37:22 +0800

    feat: 套用 canDelete canCreate

--- js/src/components/order/OrderNotes/index.tsx
+++ js/src/components/order/OrderNotes/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { Timeline, Tooltip, Input, Form, Switch, Button } from 'antd'
 import { cn, renderHTML } from 'antd-toolkit'
-import { TOrderRecord } from '@/pages/admin/Orders/List/types'
+import { TOrderRecord, TOrderNote } from '@/pages/admin/Orders/List/types'
 import { useCreate, useInvalidate } from '@refinedev/core'
 import { DeleteButton } from '@refinedev/antd'
 import dayjs from 'dayjs'
@@ -40,23 +40,25 @@ export const OrderNotes: FC<{
 						{renderHTML(content)}
 						<p className="text-xs text-gray-400 mb-0 flex items-center justify-between">
 							{date_created}{' '}
-							<DeleteButton
-								resource="order-notes"
-								recordItemId={orderNoteId}
-								type="link"
-								size="small"
-								hideText
-								confirmTitle="確認刪除此備註?"
-								confirmOkText="確認"
-								confirmCancelText="取消"
-								onSuccess={() => {
-									invalidate({
-										resource: 'orders',
-										invalidates: ['detail'],
-										id: record?.id,
-									})
-								}}
-							/>
+							{canDelete && (
+								<DeleteButton
+									resource="order-notes"
+									recordItemId={orderNoteId}
+									type="link"
+									size="small"
+									hideText
+									confirmTitle="確認刪除此備註?"
+									confirmOkText="確認"
+									confirmCancelText="取消"
+									onSuccess={() => {
+										invalidate({
+											resource: 'orders',
+											invalidates: ['detail'],
+											id: record?.id,
+										})
+									}}
+								/>
+							)}
 						</p>
 						<Tooltip
 							title={`由 ${added_by === 'system' ? '系統' : added_by} 添加`}
@@ -106,49 +108,55 @@ export const OrderNotes: FC<{
 		)
 	}
 
+	const formattedItems = canCreate
+		? [
+				{
+					children: (
+						<Form form={form} className="mb-8">
+							<Item name={['note']} noStyle>
+								<TextArea className="mb-2" rows={4} />
+							</Item>
+							<div className="flex justify-between items-center">
+								<div className="flex items-center">
+									<Item
+										name={['is_customer_note']}
+										initialValue={false}
+										noStyle
+									>
+										<Switch size="small" />
+									</Item>
+									<span className="ml-2 text-sm text-gray-400">
+										{watchIsCustomerNote
+											? '客戶備註，客戶將會看到此備註'
+											: '內部備註'}
+									</span>
+								</div>
+								<Button
+									type="primary"
+									loading={isLoading}
+									onClick={handleCreate}
+								>
+									新增
+								</Button>
+							</div>
+						</Form>
+					),
+					dot: (
+						<p className="text-xs text-right mb-0 relative right-[1rem] text-gray-400">
+							{dayjs().format('YYYY-MM-DD')}
+						</p>
+					),
+				},
+				...items,
+			]
+		: items
+
 	return (
 		<>
 			<div className="pl-10">
 				<Timeline
 					items={[
-						{
-							children: (
-								<Form form={form} className="mb-8">
-									<Item name={['note']} noStyle>
-										<TextArea className="mb-2" rows={4} />
-									</Item>
-									<div className="flex justify-between items-center">
-										<div className="flex items-center">
-											<Item
-												name={['is_customer_note']}
-												initialValue={false}
-												noStyle
-											>
-												<Switch size="small" />
-											</Item>
-											<span className="ml-2 text-sm text-gray-400">
-												{watchIsCustomerNote
-													? '客戶備註，客戶將會看到此備註'
-													: '內部備註'}
-											</span>
-										</div>
-										<Button
-											type="primary"
-											loading={isLoading}
-											onClick={handleCreate}
-										>
-											新增
-										</Button>
-									</div>
-								</Form>
-							),
-							dot: (
-								<p className="text-xs text-right mb-0 relative right-[1rem] text-gray-400">
-									{dayjs().format('YYYY-MM-DD')}
-								</p>
-							),
-						},
-						...items,
+						...formattedItems,
 						{
 							children: '',
 							dot: <></>,
@@ -163,30 +171,31 @@ export const OrderNotes: FC<{
 /**
  * 按日期分組項目
  * @param {Array} items - 包含日期的項目陣列
- * @param {string} dateField - 日期欄位的名稱，例如 'date_created'
  * @param {string} dateFormat - 日期格式，預設為 'YYYY-MM-DD'
  * @returns {Object} - 以日期為鍵的分組物件
  */
 function groupItemsByDate(
 	items: TOrderRecord['order_notes'],
-	dateField = 'date_created',
 	dateFormat = 'YYYY-MM-DD',
 ) {
 	// 使用 reduce 方法進行分組
-	const groupedItems = items?.reduce((groups, item) => {
-		// 使用 dayjs 將日期格式化為相同的格式，只保留日期部分
-		const dateKey = dayjs(item?.[dateField])?.format(dateFormat)
+	const groupedItems = items?.reduce(
+		(groups, item) => {
+			// 使用 dayjs 將日期格式化為相同的格式，只保留日期部分
+			const dateKey = dayjs(item?.date_created)?.format(dateFormat)
 
-		// 如果這個日期鍵還不存在，創建一個新陣列
-		if (!groups?.[dateKey]) {
-			groups[dateKey] = []
-		}
+			// 如果這個日期鍵還不存在，創建一個新陣列
+			if (!groups?.[dateKey]) {
+				groups[dateKey] = []
+			}
 
-		// 將項目添加到對應日期的陣列中
-		groups?.[dateKey]?.push(item)
+			// 將項目添加到對應日期的陣列中
+			groups?.[dateKey]?.push(item)
 
-		return groups
-	}, {})
+			return groups
+		},
+		{} as Record<string, TOrderNote[]>,
+	)
 
 	return groupedItems
 }
