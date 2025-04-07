a3a2beffaaea41b814de1c20a76b7bb7eb785a36
J7 <j7.dev.gg@gmail.com>
2025-04-07 19:38:05 +0800

    chore: 移除不用代碼

--- inc/classes/Utils/Base.php
+++ inc/classes/Utils/Base.php
@@ -15,7 +15,6 @@ use J7\Powerhouse\Utils\Base as PowerhouseUtils;
 abstract class Base {
 	const BASE_URL      = '/';
 	const APP1_SELECTOR = '#power_shop';
-	const APP2_SELECTOR = '#power_shop_metabox';
 	const API_TIMEOUT   = '30000';
 	const DEFAULT_IMAGE = 'http://1.gravatar.com/avatar/1c39955b5fe5ae1bf51a77642f052848?s=96&d=mm&r=g';
 
--- js/src/utils/env.tsx
+++ js/src/utils/env.tsx
@@ -9,6 +9,5 @@ export const env = simpleDecrypt(encryptedEnv)
 export const WOOCOMMERCE: TWoocommerce = window?.power_shop_data?.WOOCOMMERCE
 
 export const API_URL = env?.API_URL || '/wp-json'
-export const APP1_SELECTOR = env?.APP1_SELECTOR || 'power_docs'
-export const DOCS_POST_TYPE = env?.DOCS_POST_TYPE || 'pd_doc'
+export const APP1_SELECTOR = env?.APP1_SELECTOR || 'power_shop'
 export const DEFAULT_IMAGE = 'https://placehold.co/480x480?text=%3CIMG%20/%3E'
