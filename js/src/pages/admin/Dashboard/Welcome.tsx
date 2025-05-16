1ad06e9f93f500325e7665cd159dfd280af47271
J7 <j7.dev.gg@gmail.com>
2025-05-16 11:44:56 +0800

    feat: memo greetings

--- js/src/pages/admin/Dashboard/Welcome.tsx
+++ js/src/pages/admin/Dashboard/Welcome.tsx
@@ -1,7 +1,12 @@
+import { useMemo } from 'react'
 import { useOne } from '@refinedev/core'
-import { useEnv } from 'antd-toolkit'
 import { TUserDetails } from '@/components/user/types'
-import { getCurrentTimeSegment, getGreetings } from './utils'
+import {
+	getCurrentTimeSegment,
+	getGreetings,
+} from '@/pages/admin/Dashboard/utils'
+import { useEnv } from 'antd-toolkit'
+
 const Welcome = () => {
 	const { CURRENT_USER_ID } = useEnv()
 
@@ -10,15 +15,18 @@ const Welcome = () => {
 		id: CURRENT_USER_ID,
 	})
 
+	const { display_name = '' } = response?.data || {}
+	const { key: segmentKey } = getCurrentTimeSegment()
+	const greetings = useMemo(
+		() => getGreetings(display_name, segmentKey),
+		[display_name, segmentKey],
+	)
+
 	if (isLoading) {
 		return (
 			<div className="self-start pl-2 mb-3 w-[37.5rem] rounded-md h-8 bg-gray-200 animate-pulse"></div>
 		)
 	}
-
-	const { display_name = '' } = response?.data || {}
-	const { key: segmentKey } = getCurrentTimeSegment()
-	const greetings = getGreetings(display_name, segmentKey)
 
 	return (
 		<>
