841cccea55316677eb77ba1fb55a437eceb921b0
J7 <j7.dev.gg@gmail.com>
2025-06-03 17:05:18 +0800

    fix: 版控&打包 Power shop v2 js/dist

---
+++ legacy/js/dist/assets/App2-e3bea699.js
@@ -0,0 +1,2 @@
+import{N as r,Y as t}from"./main-6a64451b.js";import"./useAjaxNonce-66a214d0.js";import{S as i}from"./index-3d28437d.js";import"./useModal-28982b15.js";import"./useAjax-446ae264.js";function p(){return r(t,{hashPriority:"high",children:r(i,{isAdmin:!0})})}export{p as default};
+//# sourceMappingURL=App2-e3bea699.js.map
---
+++ legacy/js/dist/assets/useAjax-446ae264.js
@@ -0,0 +1,2 @@
+import{a_ as n,aA as m,bs as s}from"./useAjaxNonce-66a214d0.js";import"./main-6a64451b.js";const d=a=>{const c=n(),u=(a==null?void 0:a.args)||void 0,r=(a==null?void 0:a.config)||void 0,t=(a==null?void 0:a.mutationOptions)??{};return m({mutationFn:e=>s({args:{...e||u||{},nonce:c},config:r}),...t})};export{d as u};
+//# sourceMappingURL=useAjax-446ae264.js.map
