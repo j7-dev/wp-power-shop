c594512fe122414fd2d50db1b1858d8738865768
J7 <j7.dev.gg@gmail.com>
2025-06-05 16:13:03 +0800

    feat: 新增 admin menu bar

--- inc/classes/Admin/Entry.php
+++ inc/classes/Admin/Entry.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * Admin Entry
- */
 
 declare(strict_types=1);
 
@@ -13,26 +10,18 @@ use J7\PowerShop\Bootstrap;
 use J7\PowerShop\Utils\Base;
 use J7\Powerhouse\Utils\Base as PowerhouseBase;
 
-
-
-/**
- * Class Entry
- */
+/** Admin Entry */
 final class Entry {
 	use \J7\WpUtils\Traits\SingletonTrait;
 
-	/**
-	 * Constructor
-	 */
+	/** Constructor */
 	public function __construct() {
-		// Add the admin page for full-screen.
-		\add_action('current_screen', [ __CLASS__, 'maybe_output_admin_page' ], 10);
+		\add_action('current_screen', [ $this, 'maybe_output_admin_page' ], 10);
+		\add_action( 'admin_bar_menu', [ $this, 'admin_bar_item' ], 220 );
 	}
 
-	/**
-	 * Output the dashboard admin page.
-	 */
-	public static function maybe_output_admin_page(): void {
+	/** Output the dashboard admin page.  */
+	public function maybe_output_admin_page(): void {
 		// Exit if not in admin.
 		if (!\is_admin()) {
 			return;
@@ -47,11 +36,7 @@ final class Entry {
 		exit;
 	}
 
-	/**
-	 * Output landing page header.
-	 *
-	 * Credit: SliceWP Setup Wizard.
-	 */
+	/** Output landing page header. */
 	public static function render_page(): void {
 		Bootstrap::enqueue_script();
 		$blog_name = \get_bloginfo('name');
@@ -64,5 +49,52 @@ final class Entry {
 				'id'    => $id,
 			]
 			);
+	}
+
+	/**
+	 * 在管理員工具列中新增項目
+	 *
+	 * @param \WP_Admin_Bar $admin_bar 管理員工具列物件
+	 *
+	 * @return void
+	 */
+	public function admin_bar_item( \WP_Admin_Bar $admin_bar ): void {
+
+		if ( ! \current_user_can( 'manage_woocommerce' ) ) {
+			return;
+		}
+
+		global $product;
+		$is_product = $product instanceof \WC_Product;
+
+		if (!$is_product) {
+			// 不是商品銷售頁就顯示商品列表
+			$admin_bar->add_menu(
+				[
+					'id'     => Plugin::$kebab,
+					'parent' => null,
+					'group'  => null,
+					'title'  => '電商系統', // you can use img tag with image link. it will show the image icon Instead of the title.
+					'href'   => \admin_url('admin.php?page=power-shop#/dashboard'),
+					'meta'   => [
+						'title' => \__( '電商系統', 'power_shop' ), // This title will show on hover
+					],
+				]
+			);
+			return;
+		}
+		// 是商品銷售頁就顯示商品編輯
+		$admin_bar->add_menu(
+			[
+				'id'     => Plugin::$kebab,
+				'parent' => null,
+				'group'  => null,
+				'title'  => '編輯商品', // you can use img tag with image link. it will show the image icon Instead of the title.
+				'href'   => \admin_url("admin.php?page=power-shop#/products/edit/{$product->get_id()}"),
+				'meta'   => [
+					'title' => \__( '編輯商品', 'power_shop' ), // This title will show on hover
+				],
+			]
+		);
 	}
 }
