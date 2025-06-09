3bcb01c5792069c88f5e6eb0b5d70c94f7628c18
J7 <j7.dev.gg@gmail.com>
2025-06-09 12:23:01 +0800

    fix: 修復媒體庫只能選1張圖片的問題

--- js/src/pages/admin/WPMediaLibraryPage/index.tsx
+++ js/src/pages/admin/WPMediaLibraryPage/index.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import { MediaLibrary, TAttachment } from 'antd-toolkit/wp'
+import { MediaLibrary, TAttachment, TImage } from 'antd-toolkit/wp'
 
 export const WPMediaLibraryPage = () => {
-	const [selectedItems, setSelectedItems] = useState<TAttachment[]>([])
+	const [selectedItems, setSelectedItems] = useState<(TAttachment | TImage)[]>(
+		[],
+	)
 
 	return (
 		<div className="[&_.pc-media-library\_\_tabs\_\_filter]:top-8">
 			<MediaLibrary
 				selectedItems={selectedItems}
 				setSelectedItems={setSelectedItems}
+				limit={undefined}
 			/>
 		</div>
 	)
