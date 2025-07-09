dce7ab1a1da6ea92e33fdecb89bb333b6736c7c1
J7 <j7.dev.gg@gmail.com>
2025-07-09 23:37:41 +0800

    chore: useRecord 加上 isLoading & isFetching

--- js/src/pages/admin/Product/Edit/hooks/index.tsx
+++ js/src/pages/admin/Product/Edit/hooks/index.tsx
@@ -1,15 +1,24 @@
 import { createContext, useContext } from 'react'
 import { TProductRecord } from '@/components/product/types'
 
-export const RecordContext = createContext<TProductRecord | undefined>(
-	undefined,
-)
+export const RecordContext = createContext<
+	| (TProductRecord & {
+			isLoading: boolean
+			isFetching: boolean
+	  })
+	| undefined
+>(undefined)
 
 /**
  * 取得商品記錄
- * @returns TProductRecord | undefined
+ * @returns TProductRecord & { isLoading: boolean, isFetching: boolean } | undefined
  */
-export const useRecord = (): TProductRecord | undefined => {
+export const useRecord = ():
+	| (TProductRecord & {
+			isLoading: boolean
+			isFetching: boolean
+	  })
+	| undefined => {
 	const context = useContext(RecordContext)
 	return context
 }
