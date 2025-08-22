2c96b65de5d951c3fb8ed24863cecb4062637a5a
J7 <j7.dev.gg@gmail.com>
2025-08-22 12:27:47 +0800

    chore: 取消 legacy powershop nonce

---
+++ legacy/js/dist/assets/App2-2caf20d1.js
@@ -0,0 +1,2 @@
+import{S as r,a0 as t}from"./main-c2fc91d7.js";import"./useAjaxNonce-74bf449b.js";import{S as i}from"./index-60f35245.js";import"./useModal-b845a17d.js";import"./useAjax-5fca357c.js";function p(){return r(t,{hashPriority:"high",children:r(i,{isAdmin:!0})})}export{p as default};
+//# sourceMappingURL=App2-2caf20d1.js.map
---
+++ legacy/js/dist/assets/useAjax-5fca357c.js
@@ -0,0 +1,2 @@
+import{b2 as n,aC as m,bx as s}from"./useAjaxNonce-74bf449b.js";import"./main-c2fc91d7.js";const f=a=>{const c=n(),u=(a==null?void 0:a.args)||void 0,r=(a==null?void 0:a.config)||void 0,t=(a==null?void 0:a.mutationOptions)??{};return m({mutationFn:e=>s({args:{...e||u||{},nonce:c},config:r}),...t})};export{f as u};
+//# sourceMappingURL=useAjax-5fca357c.js.map
