bd0a59abd6c0163787ae2af711d7d91c75a4cb77
J7 <j7.dev.gg@gmail.com>
2026-02-02 19:27:26 +0800

    fix: 解決一頁商店的可變商品價格在移動時，會恢復成原始數據的問題

---
+++ legacy/js/dist/assets/App2-931fbbef.js
@@ -0,0 +1,2 @@
+import{S as r,a0 as t}from"./main-8d58a6d2.js";import"./useAjaxNonce-8673cb3b.js";import{S as i}from"./index-f8043c2a.js";import"./useModal-4d538ebc.js";import"./useAjax-1633edd7.js";function p(){return r(t,{hashPriority:"high",children:r(i,{isAdmin:!0})})}export{p as default};
+//# sourceMappingURL=App2-931fbbef.js.map
---
+++ legacy/js/dist/assets/useAjax-1633edd7.js
@@ -0,0 +1,2 @@
+import{b2 as n,aD as m,bx as s}from"./useAjaxNonce-8673cb3b.js";import"./main-8d58a6d2.js";const f=a=>{const c=n(),u=(a==null?void 0:a.args)||void 0,r=(a==null?void 0:a.config)||void 0,t=(a==null?void 0:a.mutationOptions)??{};return m({mutationFn:e=>s({args:{...e||u||{},nonce:c},config:r}),...t})};export{f as u};
+//# sourceMappingURL=useAjax-1633edd7.js.map
