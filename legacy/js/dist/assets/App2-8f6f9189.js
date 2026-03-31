a94db300b058d2b388241a589d7bbd268ed8f108
J7 <j7.dev.gg@gmail.com>
2026-03-31 13:15:40 +0800

    fix: 舊版本一頁商店可能抓不到屬性的問題

---
+++ legacy/js/dist/assets/App2-8f6f9189.js
@@ -0,0 +1,2 @@
+import{M as r,X as t}from"./main-bbc5bd67.js";import"./useAjaxNonce-6675573d.js";import{S as i}from"./index-ae931cf5.js";import"./useModal-e22a7eb1.js";import"./useAjax-0301c5d9.js";function p(){return r(t,{hashPriority:"high",children:r(i,{isAdmin:!0})})}export{p as default};
+//# sourceMappingURL=App2-8f6f9189.js.map
---
+++ legacy/js/dist/assets/useAjax-0301c5d9.js
@@ -0,0 +1,2 @@
+import{a_ as n,aA as m,bs as s}from"./useAjaxNonce-6675573d.js";import"./main-bbc5bd67.js";const d=a=>{const c=n(),u=(a==null?void 0:a.args)||void 0,r=(a==null?void 0:a.config)||void 0,t=(a==null?void 0:a.mutationOptions)??{};return m({mutationFn:e=>s({args:{...e||u||{},nonce:c},config:r}),...t})};export{d as u};
+//# sourceMappingURL=useAjax-0301c5d9.js.map
